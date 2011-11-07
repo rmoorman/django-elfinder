538b1a4aea67a9ed2b989c89326156b52c2375ca
Mike Ryan <mike@fadedink.co.uk>
2011-11-07 16:49:37 +0000

    Created docs, with very basic intro and quickstart.

--- elfinder/exceptions.py
+++
@@ -1,9 +0,0 @@
-""" elfinder Exceptions. """
-
-class InvalidTarget(Exception):
-    """ Raised when client requested an invalid File or Directory hash. 
-
-        Invalid means it does not exist, or does not belong to the current
-        tree.
-    """
-    pass
